588be7746912ff207802614a5b00dfc07755e990
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-03-11 10:00:36 -0500

    mikes trivia

---
+++ index.js
@@ -0,0 +1,46 @@
+
+let firstAnswer = window.prompt('Would you like to head left or right?')
+
+
+
+if(firstAnswer === 'left')
+{
+    let firstAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+    }
+
+else(secondAnswer === "follow")
+
+{
+    let secondAnswer  = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven. Do you stay or alert?')
+    }
+if (thirdAnswer === 'stay') 
+{
+    let thirdAnswer = window.prompt('You live happily amongst the cats for the rest of your days')
+    }
+    while(thirdAnswer === 'stay')
+    
+
+
+    (thirdAnswer != 'stay')
+{
+        let thirdAnswer = window.prompt('After living with the cat colony, you are never able to find it again; Without proof, no one believes your story, which passes into legend nonetheless')
+    }
+
+
+
+
+ 
+
+    
+if(secondAnswer === 'Continue') {
+    let secondAnswer = window.prompt ()
+}
+
+ else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
